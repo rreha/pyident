230312b81e6d764c664edc27957fce5f823d0b69
reha <74599574+rreha@users.noreply.github.com>
2022-08-18 15:06:11 +0300

    Converted mS to V

--- script.py
+++ script.py
@@ -13,16 +13,19 @@ while not osl.mustQuit():
     if not skip:
         osl.startDrawing()
         batspecs = pspos.battery()
+        batms = batspecs[6]
+        batvolt = batms / 1000
+
         msbytes = pspos.freemsspace()
         msmb = msbytes / 1024 / 1024
 
-        sfont.drawString(0, 0,  "pyident 0.1.0 by reha")
+        sfont.drawString(0, 0,  "pyident by reha")
         sfont.drawString(0, 30,  "# CPU frequency: %i MHZ" % pspos.getclock())
         sfont.drawString(0, 45,  "# BUS frequency: %i MHZ" % pspos.getbus())
         sfont.drawString(0, 60,  "# Free MS space: %i MB" % msmb)
         sfont.drawString(0, 75,  "# Remaining batt.: %i" % batspecs[3])
         sfont.drawString(0, 90,  "# Battery temp.: %i C" % batspecs[5])
-        sfont.drawString(0, 105,  "# Battery volt.: %i mS" % batspecs[6])
+        sfont.drawString(0, 105,  "# Battery volt.: %i V" % batvolt)
         sfont.drawString(0, 135,  "Press O to quit =)")
 
         osl.endDrawing()
